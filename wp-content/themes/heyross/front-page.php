62510829eb0d5468b6766eece74d17bb284d4168
heyrossbutcher <heyrossbutcher@gmail.com>
2015-06-09 22:39:10 -0400

    End of day commit

--- wp-content/themes/heyross/front-page.php
+++ wp-content/themes/heyross/front-page.php
@@ -11,45 +11,144 @@ get_header();  ?>
 
 
 	<section class="intro">
-		This is the intro copy
+		<?php //we are going to pull in the filler piece ?>
+		  <?php $latestPosts = new wp_query(array(
+		    'post_type' => 'intro',//we only want filler piece
+		    'posts_per_page' => 1
+		  )) ?> 
+		  <?php if($latestPosts->have_posts()) while($latestPosts->have_posts()) : $latestPosts->the_post() ?>
+		    <?php the_content(); ?>
+		  <?php endwhile; // end of the loop. ?>
+		  <?php wp_reset_postdata(); ?>
+		<?php //End of Thanks ?>
 	</section>
 	<!-- // -->
 	<section class="how clearfix">
-		<div class="howItWorks">
-			This is the How It Works section
+			
+		<div class="howItWorks clearfix">
+					<?php $latestPosts = new wp_query(array(
+					  'post_type' => 'how',//we only want portfolio pieces
+					  'posts_per_page' => -1
+					)) ?> 
+					<?php if($latestPosts->have_posts()) while($latestPosts->have_posts()) : $latestPosts->the_post() ?>
+						<h2><?php the_title(); ?></h2>
+						<ul>
+							<?php while( has_sub_field('how_it_works') ): ?>
+							<li class="clearfix">
+								<div class="illustration">
+									<?php $image = get_sub_field('illustration'); ?>
+								    <img src="<?php echo $image['sizes']['medium'] ?>">
+								</div>
+								<div class="description">
+									<p><?php the_sub_field('description');  //Get the Client Name ?></p>
+								</div>
+						    </li>
+						    <?php endwhile; ?>
+						</ul>
+				    <?php endwhile; ?>
 		</div>
-		<div class="weAre">
-			This is the We Are section
+		<div class="we">
+			<div class="weAre">
+					<?php $latestPosts = new wp_query(array(
+					  'post_type' => 'we_are',//we only want portfolio pieces
+					  'posts_per_page' => -1
+					)) ?> 
+					<?php if($latestPosts->have_posts()) while($latestPosts->have_posts()) : $latestPosts->the_post() ?>
+						<h2><?php the_title(); ?></h2>
+						<?php the_content(); ?>
+						<ul>
+							<?php while( has_sub_field('we_are') ): ?>
+							<li class="clearfix">
+								<div class="description">
+									<p><?php the_sub_field('list_name');  //Get the Client Name ?></p>
+								</div>
+								<div class="illustration">
+									<?php $num = get_sub_field('list_img');  //Get the Client Name ?>
+								    <img src="<?php bloginfo( 'template_url' ); ?>/img/weAre_<?php echo $num ?>.svg" alt="">
+								</div>
+						    </li>
+						    <?php endwhile; ?>
+						</ul>
+				    <?php endwhile; ?>
+			</div>
+			<div class="weAreNot clearfix">
+					<?php $latestPosts = new wp_query(array(
+					  'post_type' => 'we_are_not',//we only want portfolio pieces
+					  'posts_per_page' => -1
+					)) ?> 
+					<?php if($latestPosts->have_posts()) while($latestPosts->have_posts()) : $latestPosts->the_post() ?>
+						<h2><?php the_title(); ?></h2>
+						<?php the_content(); ?>
+						<ul>
+							<?php while( has_sub_field('list') ): ?>
+							<li class="clearfix">
+								<div class="weAreNotList">
+									<p><?php the_sub_field('list_item');  //Get the Client Name ?></p>
+								</div>
+								<div class="not">
+								<?php //$notIcon = the_sub_field('list_icon'); ?>
+								<?php if ( !$notIcon == 1 ) : ?>
+									<img src="<?php bloginfo( 'template_url' ); ?>/img/not.svg" alt="">
+								<?php endif ?>
+								</div>
+						    </li>
+						    <?php endwhile; ?>
+						</ul>
+				    <?php endwhile; ?>
+			</div>
+			
 		</div>
 	</section>
 	<!-- // -->
 	<section class="about clearfix">
+	  <?php $latestPosts = new wp_query(array(
+	    'post_type' => 'about',//we only want portfolio pieces
+	    'posts_per_page' => 1
+	  )) ?> 
+	  <?php if($latestPosts->have_posts()) while($latestPosts->have_posts()) : $latestPosts->the_post() ?>
+	    <h2><?php the_title(); ?></h2>
 		<div class="prLogo">
-			this is the parent logo
+			<?php the_post_thumbnail( "full" ); ?>
 		</div>
 		<div class="prCopy">
-			this is the parent compnay write up
+			<?php the_content(); ?> 
 		</div>
+	    
+	  <?php endwhile; // end of the loop. ?>
+	  <?php wp_reset_postdata(); ?>
+	<?php //End of grabbing Contact pieces ?>
 	</section>
 	<!-- // -->
 	<section class="partners clearfix">
-		This is the partners section
-		<ul>
-			<li>row 01</li>
-			<li>row 02</li>
-			<li>row 03</li>
-			<li>row 04</li>
-		</ul>
+		<?php $latestPosts = new wp_query(array(
+		  'post_type' => 'partners',//we only want portfolio pieces
+		  'posts_per_page' => -1
+		)) ?> 
+		<?php if($latestPosts->have_posts()) while($latestPosts->have_posts()) : $latestPosts->the_post() ?>
+			<h2><?php the_title(); ?></h2>
+			<ul>
+				<?php while( has_sub_field('partner_logos') ): ?>
+				<li>
+					<?php $image = get_sub_field('partner_logo_column'); ?>
+					 <!-- <pre><?php print_r($image);?></pre> -->
+				    <img src="<?php echo $image['sizes']['medium'] ?>">
+			    </li>
+			    <?php endwhile; ?>
+			</ul>
+	    <?php endwhile; ?>
 	</section>
 	<!-- // -->
 	<section class="contact clearfix">
-		This is the Contact section
-		<div class="cred">
-			This is for the credentials
-		</div>
-		<div class="form">
-			This is for the form
-		</div>
+		  <?php $latestPosts = new wp_query(array(
+		    'post_type' => 'contact',//we only want portfolio pieces
+		    'posts_per_page' => 1
+		  )) ?> 
+		  <?php if($latestPosts->have_posts()) while($latestPosts->have_posts()) : $latestPosts->the_post() ?>
+		    <h2><?php the_title(); ?></h2>
+		    <?php the_content(); ?> 
+		  <?php endwhile; // end of the loop. ?>
+		  <?php wp_reset_postdata(); ?>
+		<?php //End of grabbing Contact pieces ?>
 	</section>
 
 
